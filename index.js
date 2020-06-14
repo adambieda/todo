9a56c3cb04cb866dd139258bb8f66e3d13b500a1
Adam Bieda <atbieda@bu.edu>
2020-06-14 11:20:58 -0500

    saving

---
+++ index.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+
+    console.log = 'this works'
+    //give open text form
+    //collect text
+    //submit text
+    //read text
+    //group text
+    //reorganize text according to key words
+    //feed text back
+}
